56c7074c0d23d938c00736f4f69c583e94edd2ff
t0bi-0ss <183855820+t0bi-0ss@users.noreply.github.com>
2026-07-31 22:40:49 +0200

    Write required functions

---
+++ ex0/lambda_spells.py
@@ -0,0 +1,49 @@
+"""
+Demonstrates a beginner's level use of lambda functions
+"""
+
+
+def artifact_sorter(artifacts: list[dict]) -> list[dict]:
+    """
+    Returns same taken list of dicts but sorted using a combination of lambda
+    and sorted functions
+    """
+
+    return sorted(artifacts, key=lambda x: x["power"], reverse=True)
+
+
+def power_filter(mages: list[dict], min_power: int) -> list[dict]:
+    """
+    Returns the 'mages' list filtered by min_power using a combination of
+    lambda and filter functions
+    """
+
+    return list(filter(lambda x: x['power'] >= min_power, mages))
+
+
+def spell_transformer(spells: list[str]) -> list[str]:
+    """
+    Returns a list from 'spells' list adding '*' as prefix and suffix to each
+    of it's strings using a combination of lambda and map functions
+    """
+
+    return list(map(lambda x: '*' + x + '*', spells))
+
+
+def mage_stats(mages: list[dict]) -> dict:
+    """
+    Returns dict containing max, min and average power found in 'mages' using a
+    combination of lambda, max and min functions
+    """
+
+    # Using lambdas
+    max_v = max(mages, key=lambda x: x["power"])['power']
+    min_v = min(mages, key=lambda x: x["power"])['power']
+    average_v = sum(list(map(lambda x: x['power'], mages))) / len(mages)
+
+    # Using list comprehensions
+    # max_v = max(x['power'] for x in mages)
+    # min_v = min(x['power'] for x in mages)
+    # average_v = sum(x['power'] for x in mages) / len(mages)
+
+    return {'max': max_v, 'min': min_v, 'average': average_v}
